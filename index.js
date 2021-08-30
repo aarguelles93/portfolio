30e40f629f681724507068ba4e8fd97b41fccee8
Andrés Argüelles <aarguelles@uninorte.edu.co>
2021-08-29 20:50:08 -0500

    Implemented and routed views for portfolio/profile

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
 
-app.use('/', require('./routes/index'))
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
 
+app.use(express.static('assets'));
+app.use(express.urlencoded({ extended: true }));
 
-module.exports.handler = serverless(app);
+app.use('/', require('./routes/index'));
+const serverlessHandler = serverless(app);
+
+// This bypasses the issue where Serverless sends null on req.url when route is '/'
+async function handler(event, context) {
+  event.path = event.path === '' ? '/' : event.path;
+
+  const result = await serverlessHandler(event, context);
+
+  return result;
+}
+
+module.exports.handler = handler;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
+const PortfolioModel = require('../models/portfolio');
+
 router.get('/helloworld', async (req, res) => {
-  await res.send("Hello world!");
-})
+  await res.send('Hello world!');
+});
+
+router.get('/', async (req, res) => {
+  res.render('index');
+});
+
+router.get('/:userId', async (req, res) => {
+  const params = req.params;
+  const userId = params.userId;
+
+  let portfolio;
+  try {
+    portfolio = await PortfolioModel.getPortfolio(userId);
+  } catch (error) {
+    res.redirect('http://google.com');
+    return;
+  }
+
+  res.render('portfolio', portfolio);
+});
 
 module.exports = router;
